cac1a500c19191a8dc49c3f1e1298f535f879338
jacobc18 <jacob.courtney2018@utexas.edu>
2021-09-26 10:33:07 -0500

    updates windows setup batch file with more needed config

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
 ECHO running setup...
 ECHO creating .env file...
-ECHO DISCORD_TOKEN={your-discord-bot-token} > .env
-ECHO CLIENT_ID={your-discord-client-id} > .env
+(
+    ECHO DISCORD_TOKEN=your-discord-bot-token
+    ECHO CLIENT_ID=your-discord-client-id
+    ECHO YOUTUBE_API_KEY=your-youtube-api-key
+    ECHO LOGGING=0
+) > .env
 ECHO npm install
 CALL npm install
 ECHO setup complete
