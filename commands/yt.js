d0b14683b022f8eb205081c6a009904207cc6890
jacobc18 <jacob.courtney2018@utexas.edu>
2022-02-17 19:16:12 -0600

    update query link msg

--- commands/yt.js
+++ commands/yt.js
@@ -61,7 +61,7 @@ module.exports = {
 
         let query = interaction.options.getString('query');
 
-        await interaction.channel.send(`${query}`);
+        await interaction.channel.send(`\`${query}\``);
 
         const querySplit = query.split(' ');
         const shuffleFlag = querySplit[querySplit.length - 1] === '-s' || querySplit[querySplit.length - 1] === '-shuffle'
