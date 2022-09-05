a135b1b3c375242a0575ccd3f35727a995a1c8f7
jacobc18 <jacob.courtney2018@utexas.edu>
2022-09-05 11:37:08 -0500

    add unimplemented !play command

---
+++ commands/play.js
@@ -0,0 +1,15 @@
+const logger = require('../utils/logger');
+
+module.exports = {
+  data: {
+    name: 'plsy',
+    type: 'text'
+  },
+  async execute(message) {
+    const guildId = message.guildId;
+
+    logger.log(`!PLAY user: ${message.member.user.username} | guildId: ${guildId}`);
+
+    await message.channel.send('!play is not a valid command. Did you mean `/yt`?');
+  }
+};
