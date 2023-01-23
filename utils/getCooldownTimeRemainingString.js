53cc90413e63083a3250c6a4bc9d8b476e6c6dce
jacobc18 <jacob.courtney2018@utexas.edu>
2023-01-23 00:58:12 -0600

    remove console.log

--- utils/getCooldownTimeRemainingString.js
+++ utils/getCooldownTimeRemainingString.js
@@ -3,7 +3,6 @@ const { DAY_MS, HOUR_MS, MINUTE_MS, SECOND_MS } = require('../utils/constants');
 module.exports = function(currentTimestamp, lastTimestamp) {
     let hours = 0, minutes = 0, seconds = 0;
     let timeRemaining = (lastTimestamp + DAY_MS) - currentTimestamp;
-    console.log(`current: ${currentTimestamp} last:${lastTimestamp} remaining: ${timeRemaining}`);
     hours = Math.floor(timeRemaining / HOUR_MS);
     timeRemaining -= (hours * HOUR_MS);
     minutes = Math.floor(timeRemaining / MINUTE_MS);
